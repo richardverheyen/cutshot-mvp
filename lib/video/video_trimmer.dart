3e9cc869cdf2cf52334e50a8728855fef70ad3e3
Richard <richard.verheyen@gmail.com>
2023-03-22 21:38:06 +1100

    Export 2 videos from one request

--- lib/video/video_trimmer.dart
+++ lib/video/video_trimmer.dart
@@ -22,7 +22,7 @@ class VideoTrimmerWidget extends StatefulWidget {
 }
 
 class _VideoTrimmerWidgetState extends State<VideoTrimmerWidget> {
-  late File _tempVideo;
+  late List<File> _tempVideos;
   bool _isTrimming = false;
   bool _isExporting = false;
 
@@ -31,26 +31,34 @@ class _VideoTrimmerWidgetState extends State<VideoTrimmerWidget> {
       _isTrimming = true;
     });
 
-    final outputPath =
+    final outputPath1 =
+        '${(await getTemporaryDirectory()).path}/${DateTime.now().millisecondsSinceEpoch}_trimmed.mp4';
+
+    final outputPath2 =
         '${(await getTemporaryDirectory()).path}/${DateTime.now().millisecondsSinceEpoch}_trimmed.mp4';
     final duration =
         await FFprobeKit.getMediaInformation(widget.videoFile.path);
-    final startTime = 0;
-    final endTime = startTime + 4;
 
     final arguments = [
       '-y',
       '-i',
       widget.videoFile.path,
-      '-ss',
-      startTime.toString(),
-      '-t',
-      (endTime - startTime).toString(),
-      '-c',
-      'copy',
+      '-filter_complex',
+      '[0:v]trim=1:2,setpts=PTS-STARTPTS[v1];[0:v]trim=3:4,setpts=PTS-STARTPTS[v2];[0:a]atrim=1:2,asetpts=PTS-STARTPTS[a1];[0:a]atrim=3:4,asetpts=PTS-STARTPTS[a2];[v1][a1]concat=n=1:v=1:a=1[vout1][aout1];[v2][a2]concat=n=1:v=1:a=1[vout2][aout2]',
+      '-map',
+      '[vout1]',
+      '-map',
+      '[aout1]',
       '-strict',
       '-2',
-      outputPath,
+      outputPath1,
+      '-map',
+      '[vout2]',
+      '-map',
+      '[aout2]',
+      '-strict',
+      '-2',
+      outputPath2,
     ];
 
     // https://pub.dev/documentation/ffmpeg_kit_flutter/latest/ffmpeg_kit/FFmpegKit/executeAsync.html
@@ -61,11 +69,11 @@ class _VideoTrimmerWidgetState extends State<VideoTrimmerWidget> {
         print('successfully trimmed video {returnCode: $session}}');
         setState(() {
           _isTrimming = false;
-          _tempVideo = File(outputPath);
+          _tempVideos = [File(outputPath1), File(outputPath2)];
         });
         // widget.onTrimmingComplete(File(outputPath));
       } else {
-        print('failed to trim video {returnCode: $session}');
+        print('failed to trim video {returnCode: $returnCode}');
         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
           content: Text('Failed to trim video!'),
           backgroundColor: Colors.red,
@@ -77,7 +85,7 @@ class _VideoTrimmerWidgetState extends State<VideoTrimmerWidget> {
     });
   }
 
-  Future<void> _exportVideo(File exportingVideo) async {
+  Future<void> _exportVideos(List<File> exportingVideos) async {
     setState(() {
       _isExporting = true;
     });
@@ -90,32 +98,76 @@ class _VideoTrimmerWidgetState extends State<VideoTrimmerWidget> {
       appDirectory = await getApplicationDocumentsDirectory();
     }
 
-    final outputPath =
-        '${appDirectory!.path}/${DateTime.now().millisecondsSinceEpoch}_exported.mp4';
+    final List<bool> results = [];
 
-    await exportingVideo.copy(outputPath);
+    for (final video in exportingVideos) {
+      final outputPath =
+          '${appDirectory!.path}/${DateTime.now().millisecondsSinceEpoch}_exported.mp4';
 
-    final isSuccess =
-        await GallerySaver.saveVideo(outputPath, albumName: 'Cutshot Clips');
+      await video.copy(outputPath);
 
-    if (isSuccess != null) {
-      if (isSuccess) {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('Video exported successfully!'),
-          backgroundColor: Colors.green,
-        ));
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('Failed to export video!'),
-          backgroundColor: Colors.red,
-        ));
-      }
+      final isSuccess =
+          await GallerySaver.saveVideo(outputPath, albumName: 'Cutshot Clips');
 
-      setState(() {
-        _isExporting = false;
-      });
+      results.add(isSuccess ?? false);
     }
+
+    if (!results.contains(false)) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('All videos exported successfully!'),
+        backgroundColor: Colors.green,
+      ));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Failed to export one or more videos!'),
+        backgroundColor: Colors.red,
+      ));
+    }
+
+    setState(() {
+      _isExporting = false;
+    });
   }
+
+  // Future<void> _exportVideos(List<File> exportingVideo) async {
+  //   setState(() {
+  //     _isExporting = true;
+  //   });
+
+  //   late Directory? appDirectory;
+
+  //   if (Platform.isAndroid) {
+  //     appDirectory = await getExternalStorageDirectory()!;
+  //   } else {
+  //     appDirectory = await getApplicationDocumentsDirectory();
+  //   }
+
+  //   final outputPath =
+  //       '${appDirectory!.path}/${DateTime.now().millisecondsSinceEpoch}_exported.mp4';
+
+  //   await exportingVideo.copy(outputPath);
+
+  //   final isSuccess =
+  //       await GallerySaver.saveVideo(outputPath, albumName: 'Cutshot Clips');
+
+  //   if (isSuccess != null) {
+  //     if (isSuccess) {
+  //       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //         content: Text('Video exported successfully!'),
+  //         backgroundColor: Colors.green,
+  //       ));
+  //     } else {
+  //       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //         content: Text('Failed to export video!'),
+  //         backgroundColor: Colors.red,
+  //       ));
+  //     }
+
+  //     setState(() {
+  //       _isExporting = false;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -126,7 +178,7 @@ class _VideoTrimmerWidgetState extends State<VideoTrimmerWidget> {
               ? const CircularProgressIndicator()
               : const Text('Trim Video')),
       ElevatedButton(
-          onPressed: _isExporting ? null : () => _exportVideo(_tempVideo),
+          onPressed: _isExporting ? null : () => _exportVideos(_tempVideos),
           child: _isExporting
               ? const CircularProgressIndicator()
               : const Text('Export Video'))
